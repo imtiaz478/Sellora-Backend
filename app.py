e5d67da659d7c44fa212eec3e51aae179c00439e
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-04-21 21:43:22 +0600

    done

--- app.py
+++ app.py
@@ -293,9 +293,9 @@ def home():
     return {"message": "Sellora Backend Running "}
 
 
-
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+    
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
